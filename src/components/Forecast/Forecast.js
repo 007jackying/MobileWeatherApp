a5f6c8964e6db3503f995bc09fa323df35c7d7bf
Jack Lee(Teng Hong Lee) <43887352+007jackying@users.noreply.github.com>
2021-03-30 16:29:37 -0230

    Add files via upload
    src file uploading

---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Forecast = ()=>{
+
+
+    function getForecast(){
+        //fetch weather from api, data return here
+    }
+
+    return(
+        //JSX code here
+        <></>
+    )
+
+}
+
+export default Forecast;
